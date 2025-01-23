10a6b9f7ad8f55bd910d41664df4b07f8ccb01cd
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-01-23 21:52:07 +0100

    Create layout for Random Color Generator, add

---
+++ src/generate.js
@@ -0,0 +1,43 @@
+import complementaryColors from 'complementary-colors';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
+export default function RandomColorBox() {
+  const initialColor = randomColor();
+  const [rancolor, setColor] = useState(initialColor.rancolor);
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: initialColor,
+          borderRadius: '20px',
+          height: '25vh',
+          width: '50vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Generated Color:
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: '20px',
+        }}
+      >
+        <button
+          onClick={() => {
+            const newColor = randomColor();
+            setColor(newColor.rancolor);
+          }}
+        >
+          Generate
+        </button>
+      </div>
+    </> rancolor
+  );
+}
