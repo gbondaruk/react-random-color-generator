3c0ef2c5968e83a269dc182311165652f46aba4a
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-01-24 12:48:22 +0100

    Move all files to App.js; add method to generate random color, translate color to invert for text, style div, text and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
-import RandomColorBox from './generate';
+import { colord } from 'colord';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 export default function App() {
+  const [color, setColor] = useState(randomColor);
+  // Function to change color using randomColor
+  const changeColor = () => {
+    const newColor = randomColor();
+    setColor(newColor);
+  };
+
+  // Translate random color to invert for text color
+  const textColor = colord(color).invert().toHex();
+
+  // Div with generation button, text, hex value of currently displayed color
   return (
-    <div>
-      <RandomColorBox />
+    <div
+      className="App"
+      style={{
+        backgroundColor: color,
+      }}
+    >
+      <button
+        style={{
+          color: textColor,
+          boxShadow: '-1px 1px 5px black',
+        }}
+        onClick={changeColor}
+      >
+        Generate
+      </button>
+
+      <p
+        style={{
+          color: textColor,
+          textShadow: '0.5px 0.5px 1px black',
+        }}
+      >
+        Generated Color: {color}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './index.css';
-import { randomColor } from 'randomcolor';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
